2e04ab6d49d963e0f8b66d957b615bd9299b8078
ahmadml <ahmad_msalha@hotmail.com>
2021-10-31 17:02:58 +0200

    coomit

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip("how fast the object moves, in meters per second")]
+    Vector3 v = new Vector3(1,0,0);
+    
+    //void setv(int x){
+       // v.insert (x,0,0);
+   // }
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +21,16 @@ public class Oscillator : MonoBehaviour
     void Update()
     {
        Transform t =  GetComponent<Transform>();
-       t.position += new Vector3(1,0,0);
+        //t.position += v * Time.deltaTime;
+        
+            
+        for (int i = -9; i<10; i++){
+            t.position += ( v * i ) * Time.deltaTime;
+       
+        }
+      // for (int j = 0; j < 6; j++){
+              // t.position += v.setv(j,0,0) * Time.deltaTime;
+           //}
+       // t.position += v * Time.deltaTime;
     }
 }
