7b3f0edb7ba81cc35d995e0d60bb1825770b8a8e
ahmadml <ahmad_msalha@hotmail.com>
2021-10-31 23:38:49 +0200

    comit

--- Assets/Oscillator.cs
+++ Assets/Oscillator.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Oscillator : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("how fast the object moves, in meters per second")]
-    Vector3 v = new Vector3(1,0,0);
+    Vector3 v = new Vector3(30,0,0);
     
-    //void setv(int x){
-       // v.insert (x,0,0);
-   // }
-    // Start is called before the first frame update
-    void Start()
+    //float fl =(float) (Math.Sin( 90 ));
+    int ss = 0;
+        void Start()
     {
         
     }
@@ -21,16 +20,7 @@ public class Oscillator : MonoBehaviour
     void Update()
     {
        Transform t =  GetComponent<Transform>();
-        //t.position += v * Time.deltaTime;
-        
-            
-        for (int i = -9; i<10; i++){
-            t.position += ( v * i ) * Time.deltaTime;
-       
-        }
-      // for (int j = 0; j < 6; j++){
-              // t.position += v.setv(j,0,0) * Time.deltaTime;
-           //}
-       // t.position += v * Time.deltaTime;
+        //t.position += v * new Vector3((float) (Math.Sin((ss++) * (Math.PI) / 180)),0,0) * Time.deltaTime;
+        t.position +=( v * (float)(Math.Sin((ss++) * (Math.PI) / 180))) * Time.deltaTime ;
     }
 }
