2381a8299f825427f8cab042e55a9146c196eba6
ahmadml <ahmad_msalha@hotmail.com>
2021-11-02 21:47:00 +0200

    COMMIT

--- Assets/hideandshwo.cs
+++ Assets/hideandshwo.cs
@@ -1,9 +1,13 @@
+//using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class hideandshwo : MonoBehaviour
 {
+    [SerializeField]
+    [Tooltip(" ### ")]
+    float step = 1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +17,11 @@ public class hideandshwo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if  (Input.GetKeyDown(keyCode.UpArrow)){
-           // this.SetActive(false);
+        //Transform t =  GetComponent<Transform>();
+        Renderer rend = gameObject.GetComponent<Renderer>();
+        if  (Input.GetKeyDown("space")){
+            if(rend.enabled) rend.enabled = false;
+            else rend.enabled = true;
         }
     }
 }
